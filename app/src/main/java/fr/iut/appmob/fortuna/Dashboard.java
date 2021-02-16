f06da86896a09517108dc4083a7d164c7891eb46
iTrix <marwan.mezroui.pro@gmail.com>
2021-02-16 20:14:30 +0100

    add application

---
+++ app/src/main/java/fr/iut/appmob/fortuna/Dashboard.java
@@ -0,0 +1,14 @@
+package fr.iut.appmob.fortuna;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}
