7f47b503c69304b5ace5544cda44d89e3008a96e
lokioh <loiclim.etu@gmail.com>
2021-03-04 13:15:27 +0100

    re-init

---
+++ app/src/main/java/fr/iut/appmob/fortuna/MainActivity.java
@@ -0,0 +1,27 @@
+package fr.iut.appmob.fortuna;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN_TIMEOUT = 3100;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, Dashboard.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_SCREEN_TIMEOUT);
+    }
+}
