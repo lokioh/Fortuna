3bc3c9acfc8cb073943055301d28e84a8dfcfe70
iTrix <marwan.mezroui.pro@gmail.com>
2021-03-21 06:16:07 +0100

    update of views and renamed drawables of action button

--- app/src/main/java/fr/iut/appmob/fortuna/views/HomeFragment.java
+++ app/src/main/java/fr/iut/appmob/fortuna/views/HomeFragment.java
@@ -1,4 +1,4 @@
-package fr.iut.appmob.fortuna;
+package fr.iut.appmob.fortuna.views;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import fr.iut.appmob.fortuna.R;
 
 /**
  * A simple {@link Fragment} subclass.
