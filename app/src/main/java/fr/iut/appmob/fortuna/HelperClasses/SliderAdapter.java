1c43b5b40c2966d996dac76806cd4e6e90b78c92
lokioh <loiclim.etu@gmail.com>
2021-03-22 15:25:06 +0100

    add new java classes for slide & onBoarding Screen

---
+++ app/src/main/java/fr/iut/appmob/fortuna/HelperClasses/SliderAdapter.java
@@ -0,0 +1,87 @@
+package fr.iut.appmob.fortuna.HelperClasses;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+import fr.iut.appmob.fortuna.R;
+
+public class SliderAdapter extends PagerAdapter {
+
+    Context context;
+    LayoutInflater layoutInflater;
+
+    public SliderAdapter(Context context) {
+        this.context = context;
+    }
+
+    int images[] = {
+
+            R.drawable.logo_fortuna,
+            R.drawable.fleche_button,
+            R.drawable.fleche_oui,
+            R.drawable.fleche_dls,
+            R.drawable.fleche_nav_home,
+            R.drawable.fleche_stat,
+            R.drawable.fleche_setting,
+            R.drawable.fleche_icon_setting,
+            R.drawable.fleche_schedule,
+
+    };
+
+    int titles[] = {
+            R.string.first_slide_tuto_txt,
+            R.string.second_slide_tuto_txt,
+            R.string.third_slide_tuto_txt,
+            R.string.fourth_slide_tuto_txt,
+            R.string.fifth_slide_tuto_txt,
+            R.string.sixth_slide_tuto_txt,
+            R.string.seventh_slide_tuto_txt,
+            R.string.eighth_slide_tuto_txt,
+            R.string.ninth_slide_tuto_txt,
+    };
+
+
+    @Override
+    public int getCount() {
+        return titles.length;
+    }
+
+
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == (RelativeLayout) object;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+
+        layoutInflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.first_slide_on_boarding_screen, container,false);
+
+        ImageView imageView = view.findViewById(R.id.first_slide_img_on_boarding_screen);
+        TextView heading = view.findViewById(R.id.first_slide_on_boarding_screen_textView);
+
+        imageView.setImageResource(images[position]);
+        heading.setText(titles[position]);
+
+        container.addView(view);
+
+        return view;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((RelativeLayout) object);
+    }
+}
---
+++ app/src/main/java/fr/iut/appmob/fortuna/views/StartUp.java
@@ -0,0 +1,92 @@
+package fr.iut.appmob.fortuna.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import fr.iut.appmob.fortuna.HelperClasses.SliderAdapter;
+import fr.iut.appmob.fortuna.Loading;
+import fr.iut.appmob.fortuna.MainActivity;
+import fr.iut.appmob.fortuna.R;
+
+public class StartUp extends AppCompatActivity {
+
+    ViewPager viewPager;
+    LinearLayout dotsLayout;
+
+    SliderAdapter sliderAdapter;
+    TextView[] dots;
+    Button letsGetStarted;
+    int currentPosition;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_start_up);
+
+        viewPager = findViewById(R.id.slider);
+        dotsLayout = findViewById(R.id.dots);
+        letsGetStarted = findViewById(R.id.get_started_button);
+
+        sliderAdapter = new SliderAdapter(this);
+        viewPager.setAdapter(sliderAdapter);
+
+        addDots(0);
+        viewPager.addOnPageChangeListener(changeListener);
+    }
+
+    public void skip(View view) {
+        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+    }
+
+    public void next(View view){
+        viewPager.setCurrentItem(currentPosition + 1);
+    }
+
+    private void addDots(int position) {
+
+        dots = new TextView[9];
+        dotsLayout.removeAllViews();
+
+        for(int i = 0; i < dots.length; i++) {
+            dots[i] = new TextView(this);
+            dots[i].setText(Html.fromHtml("&#8226"));
+            dots[i].setTextSize(35);
+
+            dotsLayout.addView(dots[i]);
+        }
+
+        if (dots.length > 0) {
+            dots[position].setTextColor(getResources().getColor(R.color.main));
+        }
+    }
+
+    ViewPager.OnPageChangeListener changeListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            addDots(position);
+            currentPosition = position;
+
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
+}
